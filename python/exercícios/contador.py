0f0b9325ff3378755bfa0def592952b2d95d3187
Miguel Vieira Menezes da Silva <gitprogama@gmail.com>
2025-05-02 21:12:09 -0300

    contador funcionando.

--- python/exercícios/contador.py
+++ python/exercícios/contador.py
@@ -1,5 +1,11 @@
 import time
 
 print("cronometro")
-for i in range(1,75):
-    print(f"")
+
+for j in range(2,-1,-1):
+    for i in range(60,0,-1):
+        if j > 0:
+            print(f"{j} minutes, {i} seconds")
+        else:
+            print(f"{i} seconds")
+    time.sleep(1)    
