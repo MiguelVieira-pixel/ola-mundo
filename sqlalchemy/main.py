b3fed024c2ea9818ce81bc4479269b471c1d75cc
Miguel Vieira Menezes da Silva <gitprogama@gmail.com>
2025-05-14 00:34:54 -0300

    ordering query v0.1.

--- sqlalchemy/main.py
+++ sqlalchemy/main.py
@@ -9,11 +9,20 @@ class User(Base):
     __tablename__ = 'Users'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(40))
+    name = Column(String(40), unique=True)
+    age = Column(Integer)
+    def __repr__(self):
+        return f"<{self.name}>"
 
 Base.metadata.create_all(engine)
 
-with _Session as session:
-    Usuario = User(name="leugim")
-    session.add(Usuario)
-    session.commit()
+
+
+with _Session() as session:
+    usuario = User(name='Leugim', age=17)
+    session.add(usuario)
+    session.
+
+#    usuarios = session.query(User).all()
+#    for usuario in usuarios:
+#        print(usuario.name)
