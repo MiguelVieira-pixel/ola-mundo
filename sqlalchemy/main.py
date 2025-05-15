a28f181038bc50f88dcb527e2faae601efeed3a8
Miguel Vieira Menezes da Silva <gitprogama@gmail.com>
2025-05-15 16:41:31 -0300

    sqlalchemy finished .

--- sqlalchemy/main.py
+++ sqlalchemy/main.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine, Integer, String, Column
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import create_engine, Integer, String, Column, ForeignKey
+from sqlalchemy.orm import declarative_base, sessionmaker, Relationship
 
 engine = create_engine('sqlite:///dados.db')
 Base = declarative_base()
@@ -11,14 +11,29 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(40), unique=True)
     age = Column(Integer)
+
     def __repr__(self):
         return f"<{self.name}>"
+    
+    
+class Book(Base):
+    __tablename__ = 'Books'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(70))
+    year = Column(Integer)
+    author_id = Column(Integer, ForeignKey('Authors.id'))
+    author = Relationship('Author', backref='books', lazy='subquery')
+
+class Author(Base):
+    __tablename__ = 'Authors'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
 
 Base.metadata.create_all(engine)
 
 
-
 with _Session() as session:
-    usuario = session.query(User).filter_by(id=2).first()
-    session.delete(usuario)
-    session.commit()
+    autor = session.query(Author).filter_by(id=2).first()
+    print(autor.books)
