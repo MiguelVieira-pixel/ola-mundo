48f7c4c9e19d9e24ecab9cbb1c88bc311c991a67
Miguel Vieira Menezes da Silva <gitprogama@gmail.com>
2025-07-02 15:50:41 -0300

    add ep4 and teste-match

---
+++ python/guanabara-m1/ep4-ex2.py
@@ -0,0 +1,5 @@
+day = input('Digite o dia em que você nasceu: ')
+month = input('Digite o mês em que você nasceu: ')
+year = input('Digite o ano em que você nasceu: ')
+
+print(f'Você nasceu no dia {day} de {month} de {year}.')
---
+++ python/guanabara-m1/ep4-ex3.py
@@ -0,0 +1,6 @@
+num1 = input('Digite um número: ')
+num2 = input('Digite outro número: ')
+#soma = num1 + num2
+#print(f'A soma desses números é: {soma}')
+
+print(f'A soma desses números é: ',num1 + num2)
