804c08349367887834f34d09c8581d913cda2b47
Miguel Vieira Menezes da Silva <gitprogama@gmail.com>
2025-07-10 15:58:42 -0300

    Add ep6-ex2.py, organization

---
+++ python/guanabara-m1/Aula/ep4-ex3.py
@@ -0,0 +1,7 @@
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite outro número: '))
+soma = num1 + num2
+#print(f'A soma entre {num1} e {num2} é: {soma}')
+
+print('A soma entre {0} e {1} é: {2}'.format(num1, num2, soma))
+#print(type(soma))
