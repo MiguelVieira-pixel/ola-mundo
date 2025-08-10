f7dbbee6e7979117cb6b140443563ff441c53d42
Miguel Vieira <gitprogama@gmail.com>
2025-08-10 17:55:16 -0300

    add python-#005.py

---
+++ python/guanabara-m1/Exercícios/python-#005.py
@@ -0,0 +1,5 @@
+num = int(input('Digite um número: '))
+sucessor = num + 1
+antecessor = num - 1
+
+print('analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(num, antecessor, sucessor) )
