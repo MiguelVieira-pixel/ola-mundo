0336945a1eb5b676c6f3e9a1f4753b9db18f4004
Miguel Vieira <gitprogama@gmail.com>
2025-08-23 15:38:24 -0300

    add python-#006.py and 7

---
+++ python/guanabara-m1/Exercícios/python-#007.py
@@ -0,0 +1,11 @@
+media = float(0)
+
+#entrada
+nota1 = float(input('primeira nota do aluno: '))
+nota2 = float(input('segunda nota do aluno: '))
+
+#media
+media = (nota1 + nota2) / 2
+
+#saida
+print(f'A média entre {nota1:.2f} e {nota2:.2f} é igual a {media:.2f}')
