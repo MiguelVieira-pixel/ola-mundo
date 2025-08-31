1919ca8e77fcac7fc47119b07e43154ae73dbb42
Miguel Vieira <gitprogama@gmail.com>
2025-08-31 17:45:20 -0300

    add printf.c

---
+++ C/printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int x=3;
+    
+    printf("\n Qual o valor de x");
+    printf("\n x vale = %d", x);
+    printf("\n teste");
+    system("pause > null");
+}
