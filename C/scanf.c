7621e5fc29fbbabac907eeb5672778526dcce815
Miguel Vieira <gitprogama@gmail.com>
2025-08-31 18:13:04 -0300

    add scanf.c

---
+++ C/scanf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int a, b, soma=0;
+    printf("\n Entre com a: ");
+    scanf("%d", & a);
+    printf("\n entre com b: ");
+    scanf("%d", & b);
+    soma = a + b;
+    printf("\n Soma = %d", soma);
+    system("pause > null");
+}
