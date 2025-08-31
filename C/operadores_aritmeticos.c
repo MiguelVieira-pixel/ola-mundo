4d9449918d52622540f686eda117814685f75502
Miguel Vieira <gitprogama@gmail.com>
2025-08-31 17:40:01 -0300

    add operadores_aritmeticos.c

---
+++ C/operadores_aritmeticos.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int a=0,b=0,c=0,d=0;
+    a = 4;
+    b = a*2;
+    c = b/1;
+    d = c%a;
+
+    printf("\n a = %d\n b = %d\n c = %d\n d = %d",a,b,c,d);
+    system("pause > null");
+}
