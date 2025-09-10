6855111a51b46b4aac3f417f73e9225401b50970
Miguel Vieira <gitprogama@gmail.com>
2025-09-10 15:08:09 -0300

    add python-#010.py

--- python/guanabara-m1/Exercícios/python-#010.py
+++ python/guanabara-m1/Exercícios/python-#010.py
@@ -1,3 +1,4 @@
-conta = float(0)
-
-conta = input("Quanto dinheiro você tem na conta: R$")
+conta = float(input("Quanto dinheiro você tem na conta: R$"))
+#print(type(conta))
+conta = conta / 3.27
+print("Com {:.2f}".format(conta))
